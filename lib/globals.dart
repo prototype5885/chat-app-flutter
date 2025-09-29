7efdc60a6e0e1ed809b0a4250d589e6e6074271a
ProToType <prototype585@protonmail.com>
2025-09-29 03:00:17 +0200

    cors now allowed on backend

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,12 +1,8 @@
 library;
 
-import 'dart:io';
 import 'dart:ui';
-import 'package:flutter/foundation.dart' show kIsWeb;
 
-final _backendAddress = !kIsWeb && Platform.isAndroid
-    ? "10.0.2.2:3000"
-    : '127.0.0.1:3000';
+final _backendAddress = "192.168.1.230:3000";
 
 final backendHttpAddress = "http://$_backendAddress";
 final backendWsAddress = "ws://$_backendAddress/ws";
