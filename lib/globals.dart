d40f23eedede502419c8781d667124f0e7a860dc
ProToType <prototype585@protonmail.com>
2025-09-30 20:00:17 +0200

    better backend address handling

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,10 +1,14 @@
-library;
-
 import 'dart:ui';
 
-final _backendAddress = "192.168.1.230:3000";
+final String _backendProtocol = "http:";
+final String _backendAddress = "192.168.1.230";
+final String _backendPort = "3000";
 
-final backendHttpAddress = "http://$_backendAddress";
-final backendWsAddress = "ws://$_backendAddress/ws";
+final String backendHttpAddress =
+    "$_backendProtocol//$_backendAddress:$_backendPort";
+
+final String _wsProtocol = _backendProtocol == "http:" ? "ws:" : "wss:";
+final String backendWsAddress =
+    "$_wsProtocol//$_backendAddress:$_backendPort/ws";
 
 const hoverBG = Color.fromRGBO(255, 255, 255, 0.05);
